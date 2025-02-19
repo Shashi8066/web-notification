0e4fe749c8c69475d8889f9cdb5d7102c0a6e6a9
Shashi8066 <shashanksk8077@gmail.com>
2025-02-19 14:57:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    navigator.serviceWorker.register('/web-notification/firebase-messaging-sw.js')
     .then((registration) => {
         console.log('Service Worker registered with scope:', registration.scope);
     }).catch((error) => {
